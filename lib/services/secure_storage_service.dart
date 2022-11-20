d43852114be91006bad0a56ad5574045b214b0d0
CamiloAyala <camiloaayala6@gmail.com>
2022-11-20 15:20:56 -0500

    Secure storage v6 working

--- lib/services/secure_storage_service.dart
+++ lib/services/secure_storage_service.dart
@@ -1,29 +1,61 @@
 // Package imports:
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter/foundation.dart';
 
 class SecureStorageService {
   
-  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+  final storage = const FlutterSecureStorage();
 
   Future<void> storeStringValue({ required String key, required String value }) async {
-    await _storage.write(key: key, value: value);
+    await storage.write(
+      key: key,
+      value: value,
+      iOptions: _getIOSOptions(),
+      aOptions: _getAndroidOptions(),
+    );
   }
 
   Future<void> storeIntValue({ required String key, required int value }) async {
-    await _storage.write(key: key, value: value.toString());
+    await storage.write(
+      key: key,
+      value: value.toString(),
+      iOptions: _getIOSOptions(),
+      aOptions: _getAndroidOptions(),
+    );
   }
 
   Future<String?> getStringValue({ required String key }) async {
-    return await _storage.read(key: key);
+    return await storage.read(
+      key: key,
+      iOptions: _getIOSOptions(),
+      aOptions: _getAndroidOptions(),
+    );
   }
 
   Future<int?> getIntValue({ required String key }) async {
-    String? value = await _storage.read(key: key);
+    String? value = await storage.read(
+      key: key,
+      iOptions: _getIOSOptions(),
+      aOptions: _getAndroidOptions(),
+    );
+
     return value != null ? int.parse(value) : null;
   }
 
   Future<void> deleteValue({ required String key }) async {
-    await _storage.delete(key: key);
+    await storage.delete(
+      key: key,
+      iOptions: _getIOSOptions(),
+      aOptions: _getAndroidOptions(),
+    );
   }
+
+  IOSOptions _getIOSOptions() => const IOSOptions();
+
+  AndroidOptions _getAndroidOptions() => const AndroidOptions(
+    encryptedSharedPreferences: true,
+  );
+
+
 }
   
